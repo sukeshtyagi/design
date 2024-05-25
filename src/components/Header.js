7d1a58344991562dec16bfb9e69216de40373194
sukesh <sukeshtyagi05@gmail.com>
2024-05-26 04:16:53 +0530

    Header updated with click events and options showing  and hiding up

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,28 @@
-import React from "react";
+import React, { useState, useEffect, useRef } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars, faHouse } from "@fortawesome/free-solid-svg-icons";
 
 function Header() {
+  const [showHeaderOption, setShowHeaderOption] = useState(false);
+  const [showButtonOption, setShowButtonOption] = useState(false);
+
+  const headerRef = useRef(null);
+
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (headerRef.current && !headerRef.current.contains(event.target)) {
+        setShowHeaderOption(false);
+        setShowButtonOption(false);
+      }
+    };
+
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
   return (
-    <div className="headerContainer box-border w-10/12 mx-auto">
+    <div className="headerContainer  box-border w-10/12 mx-auto" ref={headerRef}>
       <div className="headerContainer box-border w-full h-[50px] mt-6 py-[26px] flex justify-between items-center">
         <div className="logoDiv box-border ">
           <img
@@ -12,28 +32,36 @@ function Header() {
           />
         </div>
 
-        <div className="svgDiv box-border block bp1:hidden cursor-pointer">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 24 24"
-            fill="currentColor"
-            class="size-8 rounded-lg text-red-400 hover:text-black hover:bg-appGreen hover:ring-offset-4 hover:ring-2 hover:ring-appGreen	"
-          >
-            <path d="M11.584 2.376a.75.75 0 0 1 .832 0l9 6a.75.75 0 1 1-.832 1.248L12 3.901 3.416 9.624a.75.75 0 0 1-.832-1.248l9-6Z" />
-            <path
-              fill-rule="evenodd"
-              d="M20.25 10.332v9.918H21a.75.75 0 0 1 0 1.5H3a.75.75 0 0 1 0-1.5h.75v-9.918a.75.75 0 0 1 .634-.74A49.109 49.109 0 0 1 12 9c2.59 0 5.134.202 7.616.592a.75.75 0 0 1 .634.74Zm-7.5 2.418a.75.75 0 0 0-1.5 0v6.75a.75.75 0 0 0 1.5 0v-6.75Zm3-.75a.75.75 0 0 1 .75.75v6.75a.75.75 0 0 1-1.5 0v-6.75a.75.75 0 0 1 .75-.75ZM9 12.75a.75.75 0 0 0-1.5 0v6.75a.75.75 0 0 0 1.5 0v-6.75Z"
-              clip-rule="evenodd"
-            />
-            <path d="M12 7.875a1.125 1.125 0 1 0 0-2.25 1.125 1.125 0 0 0 0 2.25Z" />
-          </svg>
+        <div className="svgDiv box-border relative block bp1:hidden cursor-pointer">
+          <FontAwesomeIcon
+            icon={faHouse}
+            className="box-border w-6 h-6 text-red-400"
+            onClick={() => {
+              setShowHeaderOption(!showHeaderOption);
+              setShowButtonOption(false);
+            }}
+          />
+          {showHeaderOption && (
+            <div className="headerOptions box-border w-fit absolute top-[40px] left-[-30px] px-1 rounded-lg bg-appGreen flex flex-col gap-2 text-black">
+              <p className="option box-border px-1 hover:text-red-600">Home</p>
+              <p className="option box-border px-1 hover:text-red-600">
+                Categories
+              </p>
+              <p className="option box-border px-1 hover:text-red-600">
+                Raise Startup Funds
+              </p>
+              <p className="option box-border px-1 hover:text-red-600">
+                Investors
+              </p>
+            </div>
+          )}
         </div>
 
         <div className="optionsContainer box-border cursor-pointer hidden bp1:flex text-black text-base xl1:text-xl flex justify-start items-center gap-4">
-          <p className="navItemHome box-border  px-2 hover:text-appGreen hover:border hover:border-appGreen hover:rounded-lg">
+          <p className="navItemHome box-border px-2 hover:text-appGreen hover:border hover:border-appGreen hover:rounded-lg">
             Home
           </p>
-          <p className="navItemCategories box-border  px-2 hover:text-appGreen hover:border hover:border-appGreen hover:rounded-lg">
+          <p className="navItemCategories box-border px-2 hover:text-appGreen hover:border hover:border-appGreen hover:rounded-lg">
             Categories
           </p>
           <p className="navItemfunds box-border px-2 hover:text-appGreen hover:border hover:border-appGreen hover:rounded-lg">
@@ -45,18 +73,24 @@ function Header() {
         </div>
 
         <div className="svgDiv2 box-border block sm:hidden">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 24 24"
-            fill="currentColor"
-            class="size-8 relative top-[1px] rounded-lg text-red-400 hover:text-black hover:bg-appGreen hover:ring-offset-4 hover:ring-2 hover:ring-appGreen	"
-          >
-            <path
-              fill-rule="evenodd"
-              d="M3 6.75A.75.75 0 0 1 3.75 6h16.5a.75.75 0 0 1 0 1.5H3.75A.75.75 0 0 1 3 6.75ZM3 12a.75.75 0 0 1 .75-.75h16.5a.75.75 0 0 1 0 1.5H3.75A.75.75 0 0 1 3 12Zm0 5.25a.75.75 0 0 1 .75-.75h16.5a.75.75 0 0 1 0 1.5H3.75a.75.75 0 0 1-.75-.75Z"
-              clip-rule="evenodd"
-            />
-          </svg>
+          <FontAwesomeIcon
+            icon={faBars}
+            className="box-border w-6 h-6 text-red-400"
+            onClick={() => {
+              setShowHeaderOption(false);
+              setShowButtonOption(!showButtonOption);
+            }}
+          />
+          {showButtonOption && (
+            <div className="btnOptions box-border absolute top-[70px] right-[5px] px-1 rounded-lg bg-appGreen flex flex-col gap-2 text-black">
+              <p className="option box-border px-1 hover:text-red-600">
+                Login / Sign Up
+              </p>
+              <p className="option box-border px-1 hover:text-red-600">
+                Donate Now
+              </p>
+            </div>
+          )}
         </div>
 
         <div className="btnDiv box-border hidden sm:flex text-black text-base font-normal xl1:text-xl flex justify-start items-cente gap-2">
