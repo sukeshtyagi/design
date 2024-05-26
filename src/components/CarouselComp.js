5d1aeb7ca259ec7dc92c6480e76520ea1fb6e706
sukesh <sukeshtyagi05@gmail.com>
2024-05-26 06:18:03 +0530

    Carousel implemented

--- src/components/CarouselComp.js
+++ src/components/CarouselComp.js
@@ -3,106 +3,80 @@ import Carousel from "react-bootstrap/Carousel";
 
 function CarouselComp() {
   return (
-    <Carousel indicators={true} controls={false}>
-      <Carousel.Item interval={500}>
-        <img
-          className="d-block w-100"
-          src="/images/searches/book.jpg"
-          alt="Third slide"
-        />
-        <Carousel.Caption>
-          <h3>Books are Greatest Friends</h3>
-        </Carousel.Caption>
-      </Carousel.Item>
-
-      <Carousel.Item interval={500}>
-        <img
-          className="d-block w-100"
-          src="/images/searches/book.jpg"
-          alt="Third slide"
-        />
-        <Carousel.Caption>
-          <h3>Books indeed are life changers.</h3>
-        </Carousel.Caption>
-      </Carousel.Item>
-
-      <Carousel.Item interval={500}>
-        <img
-          className="d-block w-100"
-          src="/images/searches/book.jpg"
-          alt="Third slide"
-        />
-        <Carousel.Caption>
-          <h3>Reading is more important than Food.</h3>
-        </Carousel.Caption>
-      </Carousel.Item>
-
-      <Carousel.Item interval={500}>
-        <img
-          className="d-block w-100"
-          src="/images/searches/book.jpg"
-          alt="Third slide"
-        />
-        <Carousel.Caption>
-          <h3>Books, Books and Books is all must to have.</h3>
-        </Carousel.Caption>
-      </Carousel.Item>
-
-      <Carousel.Item interval={500}>
-        <img
-          className="d-block w-100"
-          src="/images/searches/book.jpg"
-          alt="Third slide"
-        />
-        <Carousel.Caption>
-          <h3>A divine is book which followed rigorously.</h3>
-        </Carousel.Caption>
-      </Carousel.Item>
-
-      <Carousel.Item interval={500}>
-        <img
-          className="d-block w-100"
-          src="/images/searches/book.jpg"
-          alt="Third slide"
-        />
-        <Carousel.Caption>
-          <h3>God is in Books.</h3>
-        </Carousel.Caption>
-      </Carousel.Item>
-
-      <Carousel.Item interval={500}>
-        <img
-          className="d-block w-100"
-          src="/images/searches/book.jpg"
-          alt="Third slide"
-        />
-        <Carousel.Caption>
-          <h3>Books are door to prosperity.</h3>
-        </Carousel.Caption>
-      </Carousel.Item>
-
-      <Carousel.Item interval={500}>
-        <img
-          className="d-block w-100"
-          src="/images/searches/book.jpg"
-          alt="Third slide"
-        />
-        <Carousel.Caption>
-          <h3>Books are the ultimate power.</h3>
-        </Carousel.Caption>
-      </Carousel.Item>
-
-      <Carousel.Item interval={500}>
-        <img
-          className="d-block w-100"
-          src="/images/searches/book.jpg"
-          alt="Third slide"
-        />
-        <Carousel.Caption>
-          <h3>Acess to Library is a blessing.</h3>
-        </Carousel.Caption>
-      </Carousel.Item>
-    </Carousel>
+    <div style={{ maxWidth: "500px", margin: "auto" }}>
+      <Carousel indicators={true} controls={false} fade={true}>
+        <Carousel.Item interval={1500}>
+          <img
+            className="d-block w-100"
+            src="/images/searches/book7.jpg"
+            alt="one slide"
+          />
+        </Carousel.Item>
+        <Carousel.Item interval={1500}>
+          <img
+            className="d-block w-100"
+            src="/images/searches/book4.jpg"
+            alt="Two slide"
+          />
+        </Carousel.Item>
+        <Carousel.Item interval={1500}>
+          <img
+            className="d-block w-100"
+            src="/images/searches/book.jpg"
+            alt="Third slide"
+          />
+        </Carousel.Item>
+        <Carousel.Item interval={1500}>
+          <img
+            className="d-block w-100"
+            src="/images/searches/book7.jpg"
+            alt="four slide"
+          />
+        </Carousel.Item>
+        <Carousel.Item interval={1500}>
+          <img
+            className="d-block w-100"
+            src="/images/searches/book4.jpg"
+            alt="five slide"
+          />
+        </Carousel.Item>
+        <Carousel.Item interval={1500}>
+          <img
+            className="d-block w-100"
+            src="/images/searches/book.jpg"
+            alt="six slide"
+          />
+        </Carousel.Item>
+        <Carousel.Item interval={1500}>
+          <img
+            className="d-block w-100"
+            src="/images/searches/book4.jpg"
+            alt="seven slide"
+          />
+        </Carousel.Item>
+        <Carousel.Item interval={1500}>
+          <img
+            className="d-block w-100"
+            src="/images/searches/book4.jpg"
+            alt="eight slide"
+          />
+        </Carousel.Item>
+        <Carousel.Item interval={1500}>
+          <img
+            className="d-block w-100"
+            src="/images/searches/book4.jpg"
+            alt="nine slide"
+          />
+        </Carousel.Item>
+        <Carousel.Item interval={1500}>
+          <img
+            className="d-block w-100"
+            src="/images/searches/book7.jpg"
+            alt="ten slide"
+          />
+        </Carousel.Item>
+      </Carousel>
+    </div>
   );
 }
 
