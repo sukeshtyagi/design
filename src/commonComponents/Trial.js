0dc4717b11554d8f8bc14ee03e4d4037f3cb3545
sukesh <sukeshtyagi05@gmail.com>
2024-07-14 12:05:22 +0530

    Responsiveness added to Popular search comp

--- src/commonComponents/Trial.js
+++ src/commonComponents/Trial.js
@@ -2,15 +2,21 @@ import React from "react";
 import Header from "./Header";
 import Services from "../homePageComponents/Services";
 import Professions from "../homePageComponents/Professions";
+import Campaigns from "../homePageComponents/Campaigns";
+import PreviousEngagements from "../homePageComponents/PreviousEngagements";
+import Searches from "../homePageComponents/Searches";
 
 function Professions2() {
   return (
     <div style={{ overflow: "hidden" }}>
+      {/*
       <Header />
       <Services />
-      {/*
-       */}
       <Professions />
+      <Campaigns />
+      <PreviousEngagements />
+      */}
+      <Searches />
     </div>
   );
 }
